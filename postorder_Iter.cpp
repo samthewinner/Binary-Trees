6e48b4935e9fb7b181a9770af1123edbaafb0816
Samarth Pradhan <103094468+samthewinner@users.noreply.github.com>
2022-06-07 22:29:15 +0530

    Create postorder_Iter.cpp

---
+++ postorder_Iter.cpp
@@ -0,0 +1,28 @@
+void postorderIter{
+  stack<Node*>s;
+  vector<int> postorder;
+  Node* temp,*curr;
+  curr=root;
+  while(true){
+    if(curr){
+      s.push(curr);
+      curr=curr->left;
+    }
+    else{
+      temp = st.top()->right;
+      if(temp){
+        curr=temp;
+      }
+      else{
+        temp=s.top();
+        s.pop();
+        postorder.push_back(temp->data);
+        while(!s.empty() && s.top()->right == temp){
+          temp=s.top();
+          s.pop();
+          postorder.push_back(temp->data);
+        }
+      }
+    }
+  }
+}
