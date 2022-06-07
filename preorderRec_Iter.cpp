edbde672eec77611b48620a498caa1192597ec64
Samarth Pradhan <103094468+samthewinner@users.noreply.github.com>
2022-06-07 22:22:07 +0530

    Create preorderRec_Iter.cpp

---
+++ preorderRec_Iter.cpp
@@ -0,0 +1,11 @@
+void preorderIter(Node* root)
+{
+  stack<int> s;
+  s.push(root);
+  while(!s.empty()){
+    Node* temp = s.top();
+    s.pop();
+    if(temp->right)s.push(temp->right);
+    if(temp->left)s.push(temp->left);
+  }
+}
