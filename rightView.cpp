4ed82dee3f7ffff97bb2c26a869292120340429c
Samarth Pradhan <103094468+samthewinner@users.noreply.github.com>
2022-06-15 17:42:08 +0530

    Update rightView.cpp

--- rightView.cpp
+++ rightView.cpp
@@ -1,4 +1,5 @@
- vector<int> rightView(Node *root)
+// method 1: 
+vector<int> rightView(Node *root)
     {
         vector<int> ans;
         if(!root) return ans;
@@ -26,6 +27,34 @@
                    q.push(temp->left);
                }
            }
+    }
+    return ans;
+    }
+
+//method 2:
+vector<int> rightView(Node *root)
+    {
+        vector<int> ans;
+        if(!root) return ans;
+       queue<Node*> q;
+       q.push(root);
+       ans.push_back(root->data);
+       while(!q.empty()){
+           int s = q.size();
+           int tempVal=INT_MIN;
+           for(int i=0;i<s;i++)
+           {
+               Node* temp = q.front();q.pop();
+                if(temp->left){
+                    q.push(temp->left);
+                    tempVal = temp->left->data;
+                }
+                if(temp->right){
+                    q.push(temp->right);
+                    tempVal = temp->right->data;
+                }
+           }
+           if(tempVal != INT_MIN) ans.push_back(tempVal);
     }
     return ans;
     }
